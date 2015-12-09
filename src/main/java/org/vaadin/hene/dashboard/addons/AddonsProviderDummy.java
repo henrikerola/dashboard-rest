f30b8d79836f1defe1914bfb77123f826a686cdc
Henri Kerola <henri.kerola@vaadin.com>
2015-12-08 21:54:36 -0800

    Fixed dummy addons provider to respect the count argument

--- src/main/java/org/vaadin/hene/dashboard/addons/AddonsProviderDummy.java
+++ src/main/java/org/vaadin/hene/dashboard/addons/AddonsProviderDummy.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Henri Kerola / Vaadin
@@ -24,12 +25,24 @@ public class AddonsProviderDummy implements AddonsProvider {
         dummyList.add(new Addon("OpenLayers 3 Wrapper for Vaadin"));
         dummyList.add(new Addon("OpenLayers 3 Wrapper for GWT"));
         dummyList.add(new Addon("LazyTooltip Add-on"));
-//        dummyList.add(new Addon("GridUtil"));
-//        dummyList.add(new Addon("SliderPanel"));
-//        dummyList.add(new Addon("ExpandingTextArea"));
-//        dummyList.add(new Addon("LocationTextField"));
-//        dummyList.add(new Addon("GridStack Add-on"));
+        dummyList.add(new Addon("GridUtil"));
+        dummyList.add(new Addon("SliderPanel"));
+        dummyList.add(new Addon("ExpandingTextArea"));
+        dummyList.add(new Addon("LocationTextField"));
+        dummyList.add(new Addon("GridStack Add-on"));
+        dummyList.add(new Addon("VaadStrap"));
+        dummyList.add(new Addon("Sticky"));
+        dummyList.add(new Addon("Html2Canvas Screenshot"));
+        dummyList.add(new Addon("SignatureField"));
+        dummyList.add(new Addon("custommenubar"));
+        dummyList.add(new Addon("V-Leaflet-Heat"));
+        dummyList.add(new Addon("V-Leaflet"));
+        dummyList.add(new Addon("g-leaflet"));
+        dummyList.add(new Addon("Bean DataSource Spring Add-on"));
+        dummyList.add(new Addon("Bean DataSource Add-on"));
 
-        return dummyList;
+        return dummyList.stream()
+                .limit(count)
+                .collect(Collectors.toList());
     }
 }
